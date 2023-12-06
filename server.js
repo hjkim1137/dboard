7eaa75179d4d8a327bdc5c858b4eac871b66b3db
김현정(Hyunjung Kim) <withcusco@gmail.com>
2023-12-06 20:32:49 +0900

    [feat] pagenation complete

--- server.js
+++ server.js
@@ -198,3 +198,30 @@ app.delete('/delete', async (req, res) => {
     console.log(e);
   }
 });
+
+// 페이지네이션 만들기
+app.get('/list/:id', async (req, res) => {
+  // skip(5).limit(5) 맨 처음~5개 스킵하고 거기서 부터 다음 5개 가져옴
+  let result = await db
+    .collection('post')
+    .find()
+    // 1번째 페이지면 0개 스킵: 1 -> 0
+    // 2번째 페이지면 5개 스킵: 2 -> 5
+    // 3번째 페이지면 10개 스킵: 3 -> 10
+    .skip((req.params.id - 1) * 5)
+    .limit(5)
+    .toArray();
+  res.render('list.ejs', { 글목록: result });
+});
+
+// 다음 게시물 5개 보기 기능(장점: 매우 빠름, 단점: 다음 버튼으로 바꿔야함)
+app.get('/list/next/:id', async (req, res) => {
+  let result = await db
+    .collection('post')
+    // 특정 조건 다음의 게시물 5개 가져오는 방법:
+    // 형식: .find({ _id: { $gt: 방금본마지막게시물_id } })
+    .find({ _id: { $gt: new ObjectId(req.params.id) } })
+    .limit(5)
+    .toArray();
+  res.render('list.ejs', { 글목록: result });
+});
