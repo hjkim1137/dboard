e8a9e0c37d6701a51ae1f1ba84b9e27e2cee3140
김현정(Hyunjung Kim) <withcusco@gmail.com>
2024-01-30 10:43:42 +0900

    [docs] 파일 수정

--- routes/home.js
+++ routes/home.js
@@ -164,34 +164,4 @@ router.get('/tips', isLogin, async (req, res) => {
   }
 });
 
-// 페이지네이션 만들기
-// router.get('/:id', async (req, res) => {
-//   // skip(5).limit(5) 맨 처음~5개 스킵하고 거기서 부터 다음 5개 가져옴
-//   let loginuser = new ObjectId(req.user._id);
-
-//   let result = await db
-//     .collection('post')
-//     .find()
-//     // 1번째 페이지면 0개 스킵: 1 -> 0
-//     // 2번째 페이지면 5개 스킵: 2 -> 5
-//     // 3번째 페이지면 10개 스킵: 3 -> 10
-//     .skip((req.params.id - 1) * 5)
-//     .limit(5)
-//     .toArray();
-//   res.render('home.ejs', { boardPosts: result, loginUser: loginuser });
-// });
-
-// // 다음 게시물 5개 보기 기능(장점: 매우 빠름, 단점: 다음 버튼으로 바꿔야함)
-// router.get('/next/:id', async (req, res) => {
-//   let loginuser = new ObjectId(req.user._id);
-//   let result = await db
-//     .collection('post')
-//     // 특정 조건 다음의 게시물 5개 가져오는 방법:
-//     // 형식: .find({ _id: { $gt: 방금본마지막게시물_id } })
-//     .find({ _id: { $gt: new ObjectId(req.params.id) } })
-//     .limit(5)
-//     .toArray();
-//   res.render('home.ejs', { boardPosts: result, loginUser: loginuser });
-// });
-
 module.exports = router;
