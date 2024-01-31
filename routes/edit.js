7ad7096d2fe068feb96937e23c6b1f61200f6af6
김현정(Hyunjung Kim) <withcusco@gmail.com>
2024-01-31 13:47:37 +0900

    [feat] 내 댓글 삭제 기능 추가

--- routes/edit.js
+++ routes/edit.js
@@ -29,7 +29,13 @@ router.put('/', isLogin, async (req, res) => {
     await db.collection('post').updateOne(
       // 2개 조건 만족- 게시물 id와 user id를 동시에 만족하는 post를 찾아서 업데이트($set)
       { _id: new ObjectId(req.body.id), user: new ObjectId(req.user._id) },
-      { $set: { title: req.body.title, content: req.body.content } }
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.homeCategory,
+        },
+      }
     );
     res.redirect('/detail/' + req.body.id);
   } catch (e) {
@@ -38,21 +44,3 @@ router.put('/', isLogin, async (req, res) => {
 });
 
 module.exports = router;
-
-// 게시물 수정기능(DB 전송 - $inc)
-// router.put('/', isLogin, async (req, res) => {
-//   // 팁: 서버에서 정보를 찾을 수 없으면 - 유저에게 보내라고 하거나/DB에서 꺼내보거나
-//   try {
-//     await db.collection('post').updateOne({ _id: 1 }, { $inc: { like: 2 } });
-//     // $inc는 기존값에 +/- 하라는 뜻 예) 2-> +2, -2 -> -2
-//     res.redirect('/');
-//   } catch (e) {
-//     console.log(e);
-//   }
-// });
-
-// 동시에 document 여러개 수정
-// await db.collection('post').updateMany({ _id: 1 }, { $set: { like: 2 } });
-
-// 동시에 document 여러개 수정 - 조건식 입력(like가 10 이상인 것 일괄 수정)
-// await db.collection('post').updateMany({ like:{$gt:10} }, { $set: { like: 2 } });
